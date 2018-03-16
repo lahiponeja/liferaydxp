da8b3285d89330ee0575ab297772ce0cbdc22696
Manuel Mancipe <manuel.mancipe@gmail.com>
2018-03-16 09:17:40 -0500

    cambios

--- porvenir/src/main/java/co/com/general/porvenir/constants/ControlerPortletKeys.java
+++ porvenir/src/main/java/co/com/general/porvenir/constants/ControlerPortletKeys.java
@@ -4,7 +4,6 @@ package co.com.general.porvenir.constants;
  * @author Manuel
  */
 public class ControlerPortletKeys {
-
 	public static final String Controler = "Controler";
-
+	public static final String UploadFile = "UploadFile";
 }
--- porvenir/src/main/java/com/axa/portal/col/distributor/service/Provider.java
+++ porvenir/src/main/java/com/axa/portal/col/distributor/service/Provider.java
@@ -3,19 +3,23 @@ package com.axa.portal.col.distributor.service;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 
-import distribuidor.entidad.axacolpatria.cartera.v1_0.BizTalkServiceInstance;
-import distribuidor.entidad.axacolpatria.cartera.v1_0.Cartera;
-
 import java.net.MalformedURLException;
 import java.net.URL;
+
+import javax.xml.namespace.QName;
+
+import co.com.porvenir.ws.ServicioWebCesantiasAfiliados;
+import co.com.porvenir.ws.ServicioWebCesantiasAfiliadosService;
 
 public enum Provider {
 	
 	INSTANCE;
 	
+	private static final QName SERVICE_NAME = new QName("http://ws.porvenir.com.co/", "ServicioWebCesantiasAfiliadosService");
+	
 	private Log logger = LogFactoryUtil.getLog(Provider.class);
 	
-	private Cartera portCartera;
+	private ServicioWebCesantiasAfiliados portCartera;
 	
 	private Enviroment enviroment;
 	
@@ -28,7 +32,7 @@ public enum Provider {
 		}
 	}
 	
-	public Cartera getPortCartera() throws MalformedURLException {
+	public ServicioWebCesantiasAfiliados getPortCartera() throws MalformedURLException {
 		if (portCartera == null){
 			setPortPolicy();
 		}
@@ -39,8 +43,8 @@ public enum Provider {
 	private void setPortPolicy() throws MalformedURLException {
 		logger.info("url cartera " + enviroment.getServiceURLCartera());
 		URL wsdlURL = new URL(enviroment.getServiceURLCartera()); 
-		BizTalkServiceInstance serviceInstance = new BizTalkServiceInstance(wsdlURL, BizTalkServiceInstance.SERVICE);
-		this.portCartera = serviceInstance.getBasicHttpBindingITwoWayAsync();
+		ServicioWebCesantiasAfiliadosService serviceInstance = new ServicioWebCesantiasAfiliadosService(wsdlURL, SERVICE_NAME);
+		this.portCartera = serviceInstance.getServicioWebCesantiasAfiliadosPort();
 	}
 	
 }
